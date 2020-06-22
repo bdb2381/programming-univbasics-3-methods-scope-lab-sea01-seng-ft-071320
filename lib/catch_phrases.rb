1d14c4243e9c8cab62160edcd1ec2635da938efe
Brad Beggs <brad.beggs@gmail.com>
2020-06-22 21:00:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-   msg_from_Mario = puts "It's-a me, Mario"
+   msg_from_Mario = puts "It's-a me, Mario!"
    return msg_from_Mario
 end
 
